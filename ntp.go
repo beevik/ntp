003330eb6a86eaa76aae6f198491ceae223c1c5d
Christopher Batey <christopher.batey@gmail.com>
2016-05-25 14:36:22 +0100

    Offset based on rtt

--- ntp.go
+++ ntp.go
@@ -28,7 +28,11 @@ const (
 	reservedPrivate
 )
 
-const maxStratum uint8 = 16
+const (
+	frac float64 = 4294967296.0 // 2^32 as a double
+	jan1900to1970 int64 = 2208988800
+        maxStratum uint8 = 16
+)
 
 var (
 	timeout = 5 * time.Second
@@ -40,8 +44,41 @@ type ntpTime struct {
 }
 
 func (t ntpTime) UTC() time.Time {
-	nsec := uint64(t.Seconds)*1e9 + (uint64(t.Fraction) * 1e9 >> 32)
-	return time.Date(1900, 1, 1, 0, 0, 0, 0, time.UTC).Add(time.Duration(nsec))
+	return time.Date(1900, 1, 1, 0, 0, 0, 0, time.UTC).Add(time.Duration(t.nsec()))
+}
+
+func (t ntpTime) nsec() uint64 {
+	return uint64(t.Seconds) * 1e9 + (uint64(t.Fraction) * 1e9 >> 32)
+}
+
+func (a ntpTime) subtract(b ntpTime) ntpTime {
+	var result ntpTime
+	result.Fraction = a.Fraction - b.Fraction
+	result.Seconds = a.Seconds - b.Seconds
+	if result.Fraction > a.Fraction {
+		result.Seconds -= 1
+	}
+	return result
+}
+
+func (r ntpTime) add(a ntpTime) ntpTime {
+	var result ntpTime
+	result.Fraction = r.Fraction + a.Fraction
+	result.Seconds = r.Seconds + a.Seconds
+	if result.Fraction < a.Fraction {
+		result.Seconds += 1
+	}
+	return result
+}
+
+func toNtpTime(t time.Time) ntpTime {
+	seconds := uint32(t.Unix() + jan1900to1970)
+	fraction := float64(t.Nanosecond()) / 1e9
+	fraction *= frac
+	return ntpTime{
+		Seconds: seconds,
+		Fraction: uint32(fraction),
+	}
 }
 
 // msg is an internal representation of an NTP packet.
@@ -75,9 +112,32 @@ func (m *msg) SetMode(md mode) {
 	m.LiVnMode = (m.LiVnMode & 0xf8) | byte(md)
 }
 
+// SetTransmitTime sets the NTP protocol Transmit time
+func (m *msg) SetTransmitTime(t time.Time) {
+	m.TransmitTime = toNtpTime(t)
+}
+
 // Query returns information from the remote NTP server
 // specifed as host.  NTP client mode is used.
 func Query(host string, version uint8) (*Response, error) {
+	m, err := getTime(host, version)
+	if err != nil {
+		return nil, err
+	}
+	r := &Response{
+		m.Stratum,
+		m.ReceiveTime.UTC().Local(),
+	}
+	// https://tools.ietf.org/html/rfc5905#section-7.3
+	if r.Stratum == 0 {
+		r.Stratum = maxStratum
+	}
+	return r, nil
+}
+
+// Time returns the "receive time" from the remote NTP server
+// specifed as host.  NTP client mode is used.
+func getTime(host string, version byte) (*msg, error) {
 	if version < 2 || version > 4 {
 		panic("ntp: invalid version number")
 	}
@@ -97,6 +157,7 @@ func Query(host string, version uint8) (*Response, error) {
 	m := new(msg)
 	m.SetMode(client)
 	m.SetVersion(version)
+	m.SetTransmitTime(time.Now())
 
 	err = binary.Write(con, binary.BigEndian, m)
 	if err != nil {
@@ -108,30 +169,44 @@ func Query(host string, version uint8) (*Response, error) {
 		return nil, err
 	}
 
-	r := &Response{
-		m.Stratum,
-		m.ReceiveTime.UTC().Local(),
-	}
-	// https://tools.ietf.org/html/rfc5905#section-7.3
-	if r.Stratum == 0 {
-		r.Stratum = maxStratum
-	}
-	return r, nil
+	return m, nil
 }
 
 // TimeV returns the "receive time" from the remote NTP server
 // specifed as host.  Use the NTP client mode with the requested
 // version number (2, 3, or 4).
 func TimeV(host string, version byte) (time.Time, error) {
-	r, err := Query(host, version)
+	m, err := getTime(host, version)
 	if err != nil {
 		return time.Now(), err
 	}
-	return r.ReceiveTime, nil
+	return m.ReceiveTime.UTC().Local(), nil
 }
 
 // Time returns the "receive time" from the remote NTP server
 // specifed as host.  NTP client mode version 4 is used.
 func Time(host string) (time.Time, error) {
 	return TimeV(host, 4)
+}
+
+// Offset returns the offset in nanoseconds
+func Offset(host string) (uint64, error) {
+	m, err := getTime(host, 4)
+	if err != nil {
+		return 0, err
+	}
+	return offset(m.OriginTime, m.ReceiveTime, m.TransmitTime, toNtpTime(time.Now()))
+}
+
+// offset variable names based off the rfc:
+// https://tools.ietf.org/html/rfc2030
+func offset(t1, t2, t3, t4 ntpTime) (uint64, error) {
+	// https://tools.ietf.org/html/rfc2030 page 12
+	// d = (T4 - T1) - (T2 - T3)
+	// t = ((T2 - T1) + (T3 - T4)) / 2.
+	t21 := t2.subtract(t1)
+	t34 := t3.subtract(t4)
+	total := t21.add(t34)
+	t := total.nsec() / 2
+	return t, nil
 }
