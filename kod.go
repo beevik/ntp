977f5787507cbf936345da26163d30be465895e0
zhuo.meng <mengzhuo1203@gmail.com>
2017-10-06 11:05:40 +0800

    shorten the kod code

--- kod.go
+++ kod.go
@@ -11,33 +11,46 @@ See https://tools.ietf.org/html/rfc5905#section-7.4 for more detail
 
 var (
 	// ErrorACST the association belongs to a unicast server
-	ErrorACST = errors.New("the association belongs to a unicast server")
+	ErrorACST = errors.New("KOD ACST")
+
 	// ErrorAUTH server authentication failed
-	ErrorAUTH = errors.New("server authentication failed")
+	ErrorAUTH = errors.New("KOD AUTH")
+
 	// ErrorAUTO autokey sequence failed
-	ErrorAUTO = errors.New("autokey sequence failed")
+	ErrorAUTO = errors.New("KOD AUTO")
+
 	// ErrorBCST the association belongs to a broadcast server
-	ErrorBCST = errors.New("the association belongs to a broadcast server")
+	ErrorBCST = errors.New("KOD BCST")
+
 	// ErrorCRYP cryptographic authentication or identification failed
-	ErrorCRYP = errors.New("cryptographic authentication or identification failed")
+	ErrorCRYP = errors.New("KOD CRYP")
+
 	// ErrorDENY access denied by remote server
-	ErrorDENY = errors.New("access denied by remote server")
+	ErrorDENY = errors.New("KOD DENY")
+
 	// ErrorDROP lost peer in symmetric mode
-	ErrorDROP = errors.New("lost peer in symmetric mode")
+	ErrorDROP = errors.New("KOD DROP")
+
 	// ErrorRSTR access denied due to local policy
-	ErrorRSTR = errors.New("access denied due to local policy")
+	ErrorRSTR = errors.New("KOD RSTR")
+
 	// ErrorINIT the association has not yet synchronized for the first time
-	ErrorINIT = errors.New("the association has not yet synchronized for the first time")
+	ErrorINIT = errors.New("KOD INIT")
+
 	// ErrorMCST the association belongs to a dynamically discovered server
-	ErrorMCST = errors.New("the association belongs to a dynamically discovered server")
+	ErrorMCST = errors.New("KOD MCST")
+
 	// ErrorNKEY no key found
-	ErrorNKEY = errors.New("no key found")
+	ErrorNKEY = errors.New("KOD NKEY")
+
 	// ErrorRATE rate exceeded
-	ErrorRATE = errors.New("rate exceeded")
+	ErrorRATE = errors.New("KOD RATE")
+
 	// ErrorRMOT alteration of association from a remote host running ntpdc
-	ErrorRMOT = errors.New("alteration of association from a remote host running ntpdc")
+	ErrorRMOT = errors.New("KOD RMOT")
+
 	// ErrorSTEP a step change in system time has occurred, but the association has not yet resynchronized
-	ErrorSTEP = errors.New("a step change in system time has occurred, but the association has not yet resynchronized")
+	ErrorSTEP = errors.New("KOD STEP")
 
 	kodErrorMap = map[uint32]error{
 		stringToUint32("ACST"): ErrorACST,
