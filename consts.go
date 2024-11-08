1f2f4e82ae4fcc572a5ea38a769e52ada3a44cea
Mikhail <71787586+mtchuikov@users.noreply.github.com>
2024-11-08 18:50:05 +0300

    Create consts.go
    Why don't to add a set of pre-defined NTP server?

---
+++ consts.go
@@ -0,0 +1,25 @@
+package ntp
+
+const (
+	// official servers provided by ntp
+	NTPServerOfficial = "ntp.org"
+	NTPServerPool     = "pool.ntp.org"
+
+	// ntp servers of large tech companies
+	NTPServerAmazon     = "time.aws.com"
+	NTPServerCloudFlare = "time.cloudflare.com"
+	NTPServerGoogle     = "time.google.com"
+	NTPServerISC        = "time.clock.isc.org"
+	NTPServerNIST       = "time.nist.gov"
+	NTPServerUbuntu     = "ntp.ubuntu.com"
+	NTPServerWindows    = "time.windows.com"
+	NTPServerYandex     = "0.ru.pool.ntp.org"
+
+	// ntp servers by region
+	NTPServerAfrica       = "africa.pool.ntp.org"
+	NTPServerAsia         = "asia.pool.ntp.org"
+	NTPServerEurope       = "europe.pool.ntp.org"
+	NTPServerNorthAmerica = "north-america.pool.ntp.org"
+	NTPServerOceania      = "oceania.pool.ntp.org"
+	NTPServerSouthAmerica = "south-america.pool.ntp.org"
+)
