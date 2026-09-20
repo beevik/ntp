251bb5f149f6e09a241a5cf883af09c8d397e81d
Brett Vickers <brett@beevik.com>
2026-09-20 14:26:55 -0700

    Remove windows kernel timestamp support
    The code was too complicated and had a number of bugs that made
    it unworkable. Rely on the system clock on Windows.

--- conn_other.go
+++ conn_other.go
@@ -1,9 +1,8 @@
+//go:build !linux && !darwin
+
 // Copyright © Brett Vickers.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-//
-//go:build !linux && !darwin && !windows
-
 package ntp
 
 import (
